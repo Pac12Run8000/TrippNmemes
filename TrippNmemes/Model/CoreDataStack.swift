ab7c3830267ed64b6d490a9d6f00b3092f0b2bf3
Pac12Run8000 <jcmish1@gmail.com>
2018-07-31 10:27:24 -0700

    feat: I've added a new CoreDataStack file as a placeholder

---
+++ TrippNmemes/Model/CoreDataStack.swift
@@ -0,0 +1,33 @@
+//
+//  CoreDataStack.swift
+//  TrippNmemes
+//
+//  Created by Michelle Grover on 7/31/18.
+//  Copyright © 2018 Norbert Grover. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    let persistentContainer:NSPersistentContainer
+    
+    var viewContext:NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName:String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion:(() -> ())? = nil) {
+        
+        persistentContainer.loadPersistentStores { storeDescription, error in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+
+            completion?()
+        }
+    }
+}
